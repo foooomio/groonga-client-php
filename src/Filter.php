78878c4a7388ef5c9960d75c7a4b1e48d0c2e2a0
foooomio <foooomio@gmail.com>
2016-09-14 07:59:59 +0900

    Make Filter

---
+++ src/Filter.php
@@ -0,0 +1,96 @@
+<?php
+namespace MiniGroonga;
+
+class Filter
+{
+    /**
+     * Equal
+     */
+    public function eq($column, $value)
+    {
+    }
+
+    /**
+     * Not equal
+     */
+    public function ne($column, $value)
+    {
+    }
+
+    /**
+     * Alias for ne
+     */
+    public function neq($column, $value)
+    {
+    }
+
+    /**
+     * Less than
+     */
+    public function lt($column, $value)
+    {
+    }
+
+    /**
+     * Less than or equal
+     */
+    public function le($column, $value)
+    {
+    }
+
+    /**
+     * Alias for le
+     */
+    public function lte($column, $value)
+    {
+    }
+
+    /**
+     * Greater than
+     */
+    public function gt($column, $value)
+    {
+    }
+
+    /**
+     * Greater than or equal
+     */
+    public function ge($column, $value)
+    {
+    }
+
+    /**
+     * Alias for gt
+     */
+    public function gte($column, $value)
+    {
+    }
+
+    /**
+     * Partial match
+     */
+    public function match($column, $value)
+    {
+    }
+
+    /**
+     * Forward match
+     */
+    public function startWith($column, $value)
+    {
+    }
+
+    /**
+     * Backward match
+     */
+    public function endWith($column, $value)
+    {
+    }
+
+    /**
+     * Regex match
+     */
+    public function regex($column, $value)
+    {
+    }
+}
